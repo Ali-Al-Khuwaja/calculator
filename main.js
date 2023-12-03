22e75a53ab16c620a4bfe49a2802c7770acf6919
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2023-12-03 14:29:23 +0300

    fuck the war and fuck isreal , this commit  is for the calculator before rewriting the logic to make it better

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
+// equation  variables
+let a = '';
+let b = '';
+let operator = ''; 
 
-const a = '';
-const b = '';
-const operator = '';
-
-function add(a,b){return a+b;}
-function subtract(a,b){return a-b;}
-function multiply(a,b){return a+b;}
-function divide(a,b){return a/b;}
+function add(a,b){
+    a = parseInt(a);
+    b = parseInt(b);
+    screen.textContent = a+b;
+}
+function subtract(a,b){
+    a = parseInt(a);
+    b = parseInt(b);
+    screen.textContent = a-b; 
+}
+function multiply(a,b){
+    a = parseInt(a);
+    b = parseInt(b);
+    screen.textContent = a*b; 
+}
+function divide(a,b){
+    a = parseInt(a);
+    b = parseInt(b);
+    screen.textContent = a/b; 
+}
 function operate(a,operator,b){
-    if(operator===1){return add(a,b);}
-    else if(operator===2){return multiply(a,b);}
-    else if(operator===3){return divide(a,b);}
-    else if(multiply===4){return subtract(a,b);}
+    if(operator===1){add(a,b);}
+    else if(operator===2){multiply(a,b);}
+    else if(operator===3){divide(a,b);}  
+    else if(operator===4){subtract(a,b);}
 }
 const screen = document.querySelector('.screen');
 // const one = document.querySelector('#one');
@@ -25,27 +41,35 @@ const screen = document.querySelector('.screen');
 // const nine = document.querySelector('#nine');
 // const zero = document.querySelector('#zero');
 
+function reverseNumbersOnScreen(target){
+    return target.split('').reverse().join('');
+}
+
 // show what i click on calculator screen code block 
 let oldNumberOnScreen = '';
 let newNumberOnScreen = '';
 let cleared = false;
+let currentValueOnScreen = '';
 function showNumberOnScreen(number){
     if(cleared===false){
         if(delChecker===false){
             newNumberOnScreen = number+oldNumberOnScreen;
             screen.textContent = newNumberOnScreen; //display on screen entered numbers
+            currentValueOnScreen = newNumberOnScreen;
             oldNumberOnScreen = newNumberOnScreen;
-        }else if(delChecker===true){
+        }else if(delChecker===true){ // go to the delete function to understand the logic
             delChecker = false;            
-            newNumberOnScreen = '';
-            newNumberOnScreen = number + afterDelete ;
+            newNumberOnScreen = ''; // clear afterDelete value 
+            newNumberOnScreen = number + afterDelete ; // add  any entered number to afterDelete value 
             oldNumberOnScreen = newNumberOnScreen;
             screen.textContent = newNumberOnScreen;
+            currentValueOnScreen = newNumberOnScreen;
         }
-    }else if(cleared===true){
+    }else if(cleared===true){ // something is not right here , fix me later
         newNumberOnScreen = number+oldNumberOnScreen;
         oldNumberOnScreen = newNumberOnScreen;
         screen.textContent = newNumberOnScreen;
+        currentValueOnScreen = newNumberOnScreen;
         cleared= false;
         delChecker=false;
     }
@@ -58,13 +82,24 @@ function clearScreen(){
     oldNumberOnScreen = '';
     newNumberOnScreen = '';
 }
+
 let delChecker = false;
 afterDelete = '';
-function deleteLastNumber(){
+
+function deleteLastNumber(){ 
     delChecker = true;
     afterDelete = newNumberOnScreen.split("");
-    afterDelete.pop();
+    afterDelete.shift();
     afterDelete = afterDelete.join('');
     newNumberOnScreen = afterDelete;
     screen.textContent = afterDelete;
+}
+function calculatingProcessPartOne(operatorMark){
+    a = screen.textContent;
+    operator = operatorMark;
+    clearScreen();
+}
+function calculatingProcessPartTwo(){
+    b = screen.textContent;
+    operate(a,operator,b);
 }
